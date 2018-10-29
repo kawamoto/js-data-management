9cf0220dbd2c1382717aa7cf23ee155b75461932
Yuki Kawamoto <kawamoto62@gmail.com>
2018-10-29 12:50:50 +0900

    do not access to remote when data is stored in local

--- angular/src/app/data/data.service.ts
+++ angular/src/app/data/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class DataService {
@@ -8,15 +9,23 @@ export class DataService {
   source = 'https://jsonplaceholder.typicode.com/todos/1';
   constructor(private http: HttpClient) {}
 
-  getData() {
+  getData(): Observable<any> {
+    const localData = this.getDataFromLocal();
+    if (localData) {
+      return of(localData);
+    }
+    return this.getDataFromRemote();
+  }
+
+  getDataFromLocal() {
+    return JSON.parse(this.storage.getItem(this.source));
+  }
+
+  getDataFromRemote(): Observable<any> {
     return this.http.get(this.source).pipe(
       tap((data: any) => {
         this.storage.setItem(this.source, JSON.stringify(data));
       })
     );
-  }
-
-  getDataFromLocal() {
-    return JSON.parse(this.storage.getItem(this.source));
   }
 }
