4802616b7311d6682fe82c621447d5d4692530fa
Yuki Kawamoto <kawamoto62@gmail.com>
2018-10-29 12:30:18 +0900

    store data to localStorage

--- angular/src/app/data/data.service.ts
+++ angular/src/app/data/data.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class DataService {
+  storage = localStorage || window.localStorage;
   source = 'https://jsonplaceholder.typicode.com/todos/1';
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getData() {
-    return this.http.get(this.source);
+    return this.http.get(this.source).pipe(
+      tap((data: any) => {
+        this.storage.setItem(this.source, JSON.stringify(data));
+      })
+    );
   }
 }
