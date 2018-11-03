51cd38b8649ee61db2126645a108fde1b2c42b67
Yuki Kawamoto <kawamoto62@gmail.com>
2018-11-03 23:09:45 +0900

    get data from remote

--- react/src/App.js
+++ react/src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {}
+    }
+  }
+
+  getData = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(res => res.json())
+      .then(
+        (result => {
+          this.setState({
+            data: result
+          });
+        }),
+        (error => {
+          this.setState({
+            data: {}
+          });
+        })
+      )
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={this.getData}>Get Data</button>
+        <ul>
+          <li>id: {this.state.data.id}</li>
+          <li>title: {this.state.data.title}</li>
+        </ul>
       </div>
     );
   }
