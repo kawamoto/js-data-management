4932c4381f2138d94667b6097adbeb173eed72ea
Yuki <kawamoto62@gmail.com>
2025-05-01 22:20:26 +0000

    remove ngrx-component-store
    it's deprecated

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -4,7 +4,6 @@ export const routes: Routes = [
   { path: 'plain', loadComponent: () => import('./plain/plain.component').then(m => m.PlainComponent) },
   { path: 'ngrx-plain', loadChildren: () => import('./ngrx-plain/ngrx-plain.routes').then(m => m.routes) },
   { path: 'ngrx-entity', loadChildren: () => import('./ngrx-entity/ngrx-entity.routes').then(m => m.routes) },
-  { path: 'ngrx-component-store', loadComponent: () => import('./ngrx-component-store/ngrx-component-store.component').then(m => m.NgrxComponentStoreComponent) },
   { path: 'ngrx-signal-store', loadComponent: () => import('./ngrx-signal-store/ngrx-signal-store.component').then(m => m.NgrxSignalStoreComponent) },
   { path: 'ngrx-data', loadComponent: () => import('./ngrx-data/ngrx-data.component').then(m => m.NgrxDataComponent) },
 ];
